6126a5e9eb849a3c68ea21c649d1792b2212f2b1
archana prasad <archanaprasad-aspire>
2020-11-21 11:42:25 +0530

    Header Menu Fixes

--- js-storefront/spartacusstore/src/app/home-page-components/header/header-menu/header-menu.component.ts
+++ js-storefront/spartacusstore/src/app/home-page-components/header/header-menu/header-menu.component.ts
@@ -5,6 +5,7 @@ import * as $ from "jquery";
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-header-menu',
@@ -24,14 +25,21 @@ export class HeaderMenuComponent implements OnInit {
   constructor(
     protected componentData: CmsComponentData<CmsNavigationComponent>,
     protected service: NavigationService,
-    private deviceService: DeviceDetectorService
+    private deviceService: DeviceDetectorService,
+    private router: Router
   ) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationStart) {
+        this.isDropdownOpen = !this.isDropdownOpen;
+        this.openMenu();
+      }
+  });
     this.isMobile = false;
     this.epicFunction();
   }
   ngOnInit(){
     this.isDropdownOpen = false;
-    $(".SiteLogo img").unbind("click").click((event) => {
+    $(".SiteLogo a").unbind("click").click((event) => {
       this.isDropdownOpen = !this.isDropdownOpen;
       this.openMenu();
     });
@@ -56,22 +64,28 @@ export class HeaderMenuComponent implements OnInit {
   openMenu() {
     if (this.isMobile) {
         if (this.isDropdownOpen) {
-        $('.header').css("height", "90vh");
+        $('.header').css("height", "90vh");        
+        $('.SiteLogo').css("marginTop","550px");
         $('#bar-mobile').removeClass("bar-icon-mobile-menu-close");
         $('#bar-mobile').addClass("bar-icon-mobile-menu-open");
-        $('#myNav1').height("85vh"); 
+        $('#myNav1').height("80vh"); 
+        $('header').height("600px"); 
         $('body').css("overflow", "hidden");
         this.isDropdownOpen = true;
       } else {
         $('#myNav1').height("0%");
-        $('.header').css("height", "50px");
+        $('header').height("50px"); 
+        $('header').css("transition","0.5s"); 
+        $('.header').css("height", "50px");        
+        $('.SiteLogo').css("marginTop","0");
+        $('.SiteLogo').css("transition","0.3s");
         $('#ar-mobile').removeClass("bar-icon-mobile-menu-open");
         $('#bar-mobile').addClass("bar-icon-mobile-menu-close");
         $('body').css("overflow", "auto");
         this.isDropdownOpen = false;
       }
     } else {
-      $('#myNav1').css("height", "100vh");
+      $('#myNav1').css("height", "100vh"); 
     }
   }
   closeMenu() {
