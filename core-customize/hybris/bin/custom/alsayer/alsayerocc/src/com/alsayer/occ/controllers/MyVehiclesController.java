60a235c4002434af0d9e7b716a7a432913f381f5
Krishnakumar Raju <krishna.kumar@aspiredigital.com>
2020-12-07 01:27:52 +0530

    Vehicle data display in controller fixed

--- core-customize/hybris/bin/custom/alsayer/alsayerocc/src/com/alsayer/occ/controllers/MyVehiclesController.java
+++ core-customize/hybris/bin/custom/alsayer/alsayerocc/src/com/alsayer/occ/controllers/MyVehiclesController.java
@@ -8,22 +8,22 @@ package com.alsayer.occ.controllers;
 
 import com.alsayer.facades.data.VehicleData;
 import com.alsayer.facades.vehicles.MyVehiclesFacade;
-import com.alsayer.facades.vehicles.impl.DefaultMyVehiclesFacade;
 import com.alsayer.occ.dto.VehicleListWsDTO;
 import com.alsayer.occ.dto.VehicleWsDTO;
 import com.alsayer.occ.dto.WarrantyWsDTO;
 import de.hybris.platform.webservicescommons.mapping.DataMapper;
-import de.hybris.platform.webservicescommons.mapping.FieldSetLevelHelper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.LinkedList;
@@ -54,25 +54,21 @@ public class MyVehiclesController {
     @ApiOperation(value = "Get Vehicles of Customer", notes = "Only registered users can get vehicles")
     public VehicleListWsDTO getVehicles(@ApiParam(value = "Response configuration. This is the list of fields that should be returned in the response body.", allowableValues = "BASIC, DEFAULT, FULL")
                                         @RequestParam(defaultValue = BASIC_FIELD_SET) final String fields) {
-        List<VehicleData> dataList = myVehiclesFacade.getVehicles();
-            return getVehicleWsDtoList(dataList);
-
+        return getVehicleWsDtoList(fields);
     }
 
-    private VehicleListWsDTO getVehicleWsDtoList(List<VehicleData> dataList) {
-        VehicleListWsDTO vehicleList = new VehicleListWsDTO();
-        VehicleWsDTO vehicleWsDTO =new VehicleWsDTO();
-        List<VehicleWsDTO> vehicleWsDTOList=new LinkedList<>();
-        for(VehicleData data : dataList) {
-            vehicleWsDTO.setChassisNumber(data.getChassisNumber());
-            vehicleWsDTO.setModline(data.getModline());
-            vehicleWsDTO.setModyear(data.getModyear());
-            vehicleWsDTO.setPlateNumber(data.getPlateNumber());
-            if(CollectionUtils.isNotEmpty(data.getWarranties())) {
-                vehicleWsDTO.setWarranties(dataMapper.mapAsList(data.getWarranties(),WarrantyWsDTO.class, FieldSetLevelHelper.DEFAULT_LEVEL));
+    private VehicleListWsDTO getVehicleWsDtoList(final String fields) {
+        final List<VehicleData> dataList = myVehiclesFacade.getVehicles();
+        final VehicleListWsDTO vehicleList = new VehicleListWsDTO();
+        final List<VehicleWsDTO> vehicleWsDTOList = new LinkedList<>();
+        dataList.forEach(x -> {
+            final VehicleWsDTO vehicleWsDTO = dataMapper.map(x, VehicleWsDTO.class, fields);
+            if (x.getWarranties() != null)
+            {
+                vehicleWsDTO.setWarranties(dataMapper.mapAsList(x.getWarranties(), WarrantyWsDTO.class, fields));
             }
             vehicleWsDTOList.add(vehicleWsDTO);
-        }
+        });
         vehicleList.setVehicleList(vehicleWsDTOList);
         return vehicleList;
     }
