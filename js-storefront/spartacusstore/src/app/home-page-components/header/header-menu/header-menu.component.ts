b95488cc281c9d85a0a3d7d6699115f32c26ae2b
ASPIRE-ANKUSH <ASPIRE-ANKUSH@DESKTOP-65CKTO0>
2020-12-21 02:46:38 +0530

    Profile photo code merged

--- js-storefront/spartacusstore/src/app/home-page-components/header/header-menu/header-menu.component.ts
+++ js-storefront/spartacusstore/src/app/home-page-components/header/header-menu/header-menu.component.ts
@@ -23,10 +23,12 @@ declare var $: any;
   animations: [
     trigger('slideInOut', [
       state('in', style({
-        transform: 'translate3d(0, 0, 0)'
+        // transform: 'translate3d(0, 0, 0)'
+        display: 'block'
       })),
       state('out', style({
-        transform: 'translate3d(-100%, 0, 0)'
+        // transform: 'translate3d(-100%, 0, 0)'
+        display: 'none'
       })),
       transition('in => out', animate('400ms ease-in-out')),
       transition('out => in', animate('400ms ease-in-out'))
