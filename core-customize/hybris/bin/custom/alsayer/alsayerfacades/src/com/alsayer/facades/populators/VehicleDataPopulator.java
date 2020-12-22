f0edb49cf53e345d7cbbcea097e04eb31fb7b075
ASPIRE-ANKUSH <ASPIRE-ANKUSH@DESKTOP-65CKTO0>
2020-12-22 16:57:50 +0530

    Added Warranty converter

--- core-customize/hybris/bin/custom/alsayer/alsayerfacades/src/com/alsayer/facades/populators/VehicleDataPopulator.java
+++ core-customize/hybris/bin/custom/alsayer/alsayerfacades/src/com/alsayer/facades/populators/VehicleDataPopulator.java
@@ -6,12 +6,16 @@ import com.alsayer.facades.data.VehicleData;
 import com.alsayer.facades.data.WarrantyData;
 import de.hybris.platform.converters.Populator;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
 import org.apache.commons.collections.CollectionUtils;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class VehicleDataPopulator implements Populator<VehicleModel, VehicleData> {
+
+    Converter<WarrantyModel,WarrantyData> warrantyConverter;
+
     @Override
     public void populate(VehicleModel source, VehicleData target) throws ConversionException {
         if(source!=null){
@@ -20,16 +24,17 @@ public class VehicleDataPopulator implements Populator<VehicleModel, VehicleData
             target.setModyear(source.getModyear());
             target.setStatus(source.getStatus());
             target.setChassisNumber(source.getChassisNumber());
-            List<WarrantyData> warrantyDataList=new LinkedList<>();
             if(CollectionUtils.isNotEmpty(source.getWarranties())){
-                for(WarrantyModel warranty: source.getWarranties()){
-                    WarrantyData data=new WarrantyData();
-                    data.setWarrantyType(warranty.getWarrantyType());
-                    data.setWarrantyExpiryDate(warranty.getWarrantyExpiryDate());
-                    warrantyDataList.add(data);
-                }
-                target.setWarranties(warrantyDataList);
+                target.setWarranties(warrantyConverter.convertAll(source.getWarranties()));
             }
         }
+    }
+
+    public Converter<WarrantyModel, WarrantyData> getWarrantyConverter() {
+        return warrantyConverter;
+    }
+
+    public void setWarrantyConverter(Converter<WarrantyModel, WarrantyData> warrantyConverter) {
+        this.warrantyConverter = warrantyConverter;
     }
 }
--- core-customize/hybris/bin/custom/alsayer/alsayerfacades/src/com/alsayer/facades/populators/VehicleDataReversePopulator.java
+++ core-customize/hybris/bin/custom/alsayer/alsayerfacades/src/com/alsayer/facades/populators/VehicleDataReversePopulator.java
@@ -1,11 +1,18 @@
 package com.alsayer.facades.populators;
 
 import com.alsayer.core.model.VehicleModel;
+import com.alsayer.core.model.WarrantyModel;
 import com.alsayer.facades.data.VehicleData;
+import com.alsayer.facades.data.WarrantyData;
 import de.hybris.platform.converters.Populator;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
+import org.apache.commons.collections.CollectionUtils;
 
 public class VehicleDataReversePopulator implements Populator<VehicleData, VehicleModel> {
+
+    Converter<WarrantyData, WarrantyModel> warrantyReverseConverter;
+
     @Override
     public void populate(VehicleData vehicleData, VehicleModel vehicleModel) throws ConversionException {
 
@@ -15,5 +22,17 @@ public class VehicleDataReversePopulator implements Populator<VehicleData, Vehic
         vehicleModel.setModyear(vehicleData.getModyear());
         vehicleModel.setPlateNumber(vehicleModel.getPlateNumber());
 
+        if(CollectionUtils.isNotEmpty(vehicleData.getWarranties())){
+            vehicleModel.setWarranties(warrantyReverseConverter.convertAll(vehicleData.getWarranties()));
+        }
+
+    }
+
+    public Converter<WarrantyData, WarrantyModel> getWarrantyReverseConverter() {
+        return warrantyReverseConverter;
+    }
+
+    public void setWarrantyReverseConverter(Converter<WarrantyData, WarrantyModel> warrantyReverseConverter) {
+        this.warrantyReverseConverter = warrantyReverseConverter;
     }
 }
